1f6a76fb7f7be3543d93b5b4bbd3b404e6cf4c2a
arash <arash.tayebi00@gmail.com>
2024-10-17 20:16:16 +0330

    Add types directory and weather api type

--- src/lib/api/weather.ts
+++ src/lib/api/weather.ts
@@ -1,13 +1,15 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
+import { WeatherData } from "@/src/types/weatherData";
+
 export const weatherApi = createApi({
   reducerPath: "weatherApi",
   baseQuery: fetchBaseQuery({
     baseUrl: `${import.meta.env.VITE_API_END_POINT}`,
   }),
   endpoints: (builder) => ({
-    getWeather: builder.query({
-      query: ({ location, days }: { location: string; days: number }) =>
+    getWeather: builder.query<WeatherData, { location: string; days: number }>({
+      query: ({ location, days }) =>
         `/forecast.json?key=${import.meta.env.VITE_API_KEY}&days=${days}&q=${location}`,
     }),
   }),
---
+++ src/types/weatherData.ts
@@ -0,0 +1,52 @@
+import { ForecastDay } from "./forecastDay";
+
+export type WeatherData = {
+  location: {
+    country: string;
+    lat: number;
+    localtime: string;
+    localtime_epoch: number;
+    lon: number;
+    name: string;
+    region: string;
+    tz_id: string;
+  };
+  current: {
+    cloud: number;
+    condition: {
+      text: string;
+      icon: string;
+      code: number;
+    };
+    dewpoint_c: number;
+    dewpoint_f: number;
+    feelslike_c: number;
+    feelslike_f: number;
+    gust_kph: number;
+    gust_mph: number;
+    heatindex_c: number;
+    heatindex_f: number;
+    humidity: number;
+    is_day: number;
+    last_updated: string;
+    last_updated_epoch: number;
+    precip_in: number;
+    precip_mm: number;
+    pressure_in: number;
+    pressure_mb: number;
+    temp_c: number;
+    temp_f: number;
+    uv: number;
+    vis_km: number;
+    vis_miles: number;
+    wind_degree: number;
+    wind_dir: string;
+    wind_kph: number;
+    wind_mph: number;
+    windchill_c: number;
+    windchill_f: number;
+  };
+  forecast: {
+    forecastday: ForecastDay[];
+  };
+};
