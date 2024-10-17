22c8cc182e51c817304fb51749f4927cf6ad470e
arash <arash.tayebi00@gmail.com>
2024-10-17 18:28:16 +0330

    Edit weather api

--- src/lib/api/weather.ts
+++ src/lib/api/weather.ts
@@ -2,10 +2,13 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const weatherApi = createApi({
   reducerPath: "weatherApi",
-  baseQuery: fetchBaseQuery({ baseUrl: `${import.meta.env.VITE_API_END_POINT}` }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: `${import.meta.env.VITE_API_END_POINT}`,
+  }),
   endpoints: (builder) => ({
-    getWeather: builder.query<number, number>({
-      query: (days) => `/forecast.json?key=${import.meta.env.VITE_API_KEY}&days=${days}&q=iran`,
+    getWeather: builder.query({
+      query: ({ location, days }: { location: string; days: number }) =>
+        `/forecast.json?key=${import.meta.env.VITE_API_KEY}&days=${days}&q=${location}`,
     }),
   }),
 });
